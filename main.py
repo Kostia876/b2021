5ca9aa1bc42d983b9161ad3696605a905dbd2514
kostia876 <kostia.furman7@gmail.com>
2023-05-28 10:07:02 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,47 +1,24 @@
-import random
-class Cat:
-    def __init__(self,name):
-        self.name=name
-        self.gladness=20
-        self.food=10
-        self.alive=True
-    def to_run(self):
-        print("час бігати!")
-        self.food-=0.12
-        self.gladness+=3
-    def to_chill(self):
-        print("час відпочити!")
-        self.gladness+=2
-        self.food-=0.1
-    def to_sleep(self):
-        print("час спати")
-    def is_alive(self):
-        if self.food<-0.5:
-            print("втікли...")
-            self.alive=False
-        elif self.gladness<=0:
-            print("скучно...")
-            self.alive=False
-        elif self.food>50:
-            print("ви щясливі!")
-            self.alive=False
-    def end_of_day(self):
-        print(f"Gladness - {self.gladness}")
-        print(f"food - {round(self.food,2)}")
-    def live(self,day):
-        day="Day" + str(day) + "of" + self.name +"life"
-        print(f"{day:=^50}")
-        live_cube=random.randint(1,3)
-        if live_cube==1:
-            self.to_run()
-        elif live_cube==2:
-            self.to_sleep()
-        elif live_cube==3:
-            self.to_chill()
-        self.end_of_day()
-        self.is_alive()
-nick=Cat(name="nono")
-for day in range(365):
-    if nick.alive==False:
-        break
-    nick.live(day)
+from colorama import *
+
+
+init()
+from colorama import Fore, Back, Style
+print(Fore.RED + 'maximal')
+print(Back.YELLOW + 'maximal')
+print(Style.DIM + 'maximal')
+print(Style.RESET_ALL)
+print('maximal?')
+
+print('\033[31m' + ' red text')
+print('\033[39m')
+
+
+print(Back.BLUE + 'maximal')
+print(Fore.YELLOW + 'maximal')
+print('\033[31m' + 'maximal')
+print('\033[39m') # and reset to default color
+init(autoreset=True)
+print(Fore.RED + ' red text')
+print('???')
+
+
